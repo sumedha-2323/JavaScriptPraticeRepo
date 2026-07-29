61c376e9fa76631f13cc4fb07c860372d6bbfe52
sumedha-2323 <120385613+sumedha-2323@users.noreply.github.com>
2026-07-29 10:03:35 +0530

    strings

---
+++ demo-creatingStrings.js
@@ -0,0 +1,5 @@
+let name="sumedha";
+// let name = 'sumedha';
+console.log(name);
+let greet=`Hello ${name}`;
+console.log(greet)
---
+++ demo-stringMethods.js
@@ -0,0 +1,15 @@
+let name="Sumedha";
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name[2]);
+console.log(name.indexOf("h"));
+console.log(name.includes("Sum"));
+console.log(name.startsWith("s"));
+console.log(name.endsWith("b"));
+console.log(name.replace("Sumedha", "Sumedha Jammula"));
+console.log(name.substring(0,4));
+
+let text ="   apple,banana,mango   ";
+console.log(text.split(","));
+console.log(text.trim());
